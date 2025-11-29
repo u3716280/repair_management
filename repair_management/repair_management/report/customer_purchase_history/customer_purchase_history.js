bff098d8c3d1dd54bd5b4ddc034b68c2a8f69737
Chirayut Deecharoen <u3716280@eakthai.com>
2025-11-29 16:54:41 +0700

    Update

---
+++ repair_management/repair_management/report/customer_purchase_history/customer_purchase_history.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Chirayut D. and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Customer Purchase History"] = {
+	"filters": [
+
+	]
+};
