435d396bbc547d51da3dd5e57ba6abfca5d2541f
Chirayut Deecharoen <u3716280@eakthai.com>
2025-12-02 13:20:18 +0700

    Update

--- repair_management/repair_management/report/customer_purchase_history/customer_purchase_history.js
+++ repair_management/repair_management/report/customer_purchase_history/customer_purchase_history.js
@@ -1,35 +1,46 @@
-// Copyright (c) 2025, Chirayut D. and contributors
-// For license information, please see license.txt
-
-// frappe.query_reports["Customer Purchase History"] = {
-//	"filters": [
-
-//	]
-// };
-
 frappe.query_reports["Customer Purchase History"] = {
-    "filters": [
+    filters: [
         {
             fieldname: "customer",
-            label: "Customer",
+            label: __("Customer"),
             fieldtype: "Link",
-            options: "Customer",
- 	    reqd: 1,
+            options: "Customer"
         },
         {
             fieldname: "from_date",
-            label: "From Date",
+            label: __("From Date"),
             fieldtype: "Date",
-            default: frappe.datetime.year_start(),
-            reqd: 1,
+            default: frappe.datetime.year_start()
         },
         {
             fieldname: "to_date",
-            label: "To Date",
+            label: __("To Date"),
             fieldtype: "Date",
-            default: frappe.datetime.get_today(),
-            reqd: 1,
+            default: frappe.datetime.get_today()
         }
-    ]
+    ],
+
+    onload: function(report) {
+        report.page.add_inner_button(__("Open Calendar View"), () => {
+            const filters = report.get_filter_values() || {};
+
+            let route_options = {};
+
+            // ถ้าเลือก customer → ส่งไปเป็น filter ให้ Calendar
+            if (filters.customer) {
+                route_options.customer = filters.customer;
+            }
+
+            // ใช้ posting_date ระหว่าง from_date – to_date
+            // เฉพาะกรณีที่มีทั้ง 2 ค่า และไม่ใช่ค่าว่าง
+            // if (filters.from_date && filters.to_date) {
+            //    route_options.posting_date = ['between', filters.from_date, filters.to_date];
+            //}
+
+            // ตั้ง route_options แล้วเปิด Calendar View ของ Sales Invoice
+            frappe.route_options = route_options;
+            frappe.set_route('List', 'Sales Invoice', 'Calendar');
+        });
+    }
 };
 
