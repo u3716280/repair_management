5ca3d79d7ac1f7941d75221393df749cec745b57
Chirayut Deecharoen <u3716280@eakthai.com>
2025-12-28 16:37:01 +0700

    Update

---
+++ repair_management/portfolio/doctype/portfolio_stock_name/portfolio_stock_name.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Chirayut D. and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Portfolio Stock Name", {
+// 	refresh(frm) {
+
+// 	},
+// });
